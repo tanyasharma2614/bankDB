ddf801d33b7971121c20d52246524f352965fe7d
Maharshi Patel <65079977+maharshipatel1@users.noreply.github.com>
2023-11-25 18:46:28 -0500

    Added the data file to populate initial resources

---
+++ data.sql
@@ -0,0 +1,5 @@
+USE bank;
+INSERT INTO Customer (Customer_Id, First_Name, Last_Name, Address, Cell_Phone, Email, DOB) Values (1, 'Maharshi', 'Patel', 'NJ', '123-456-7890', 'noreply@gmail.com', '1909-01-01');
+
+INSERT INTO Accounts (Account_Number, Date_Of_Creation, Customer_Id, Account_Balance, Account_Type) Values (101, '2023-10-10', 1, 1000, '0');
+INSERT INTO Accounts (Account_Number, Date_Of_Creation, Customer_Id, Account_Balance, Account_Type) Values (102, '2023-10-10', 1, 1000, '2');
